aa84a8b19f2492293ee28d45729ba5ae49468f16
rarhs <reza054@gmail.com>
2015-10-06 08:05:23 +0000

    Solves both exercises

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "Reza's experimental app"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Reza's experimental app"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Reza's experimental app"
+    assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Reza's experimental app"
+    assert_select "title", "About | #{@base_title}"
+  end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
