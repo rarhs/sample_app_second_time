a6206b0c4d3ca6efd6534155390a1a9a393ff7e5
rarhs <reza054@gmail.com>
2015-10-09 10:16:38 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,5 +18,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Reza's experimental app"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Reza's experimental app"
+  end
 
 end
