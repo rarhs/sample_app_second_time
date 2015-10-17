012ff3c694bfda430dfffa5242751c7425e82237
rarhs <reza054@gmail.com>
2015-10-17 20:18:02 +0000

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,5 +22,6 @@ class UserSignupTest < ActionDispatch::IntegrationTest
                               password_confirmation:  "password"}
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
